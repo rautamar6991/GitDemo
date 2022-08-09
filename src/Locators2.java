87e128ec6c28d4f67eb19de0530e701862a8dbf1
Amar <rautamar6991@gmail.com>
2022-08-10 02:34:24 +0530

    first project with git

---
+++ src/Locators2.java
@@ -0,0 +1,24 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Locators2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:/Selenium Study/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("amar@gmail.com");
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");;
+		driver.findElement(By.className("signInBtn")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.findElement(By.tagName("p")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are  logged in.");
+	}
+
+}
+ 
